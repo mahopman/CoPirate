04cae1ea4a073da1c4266d433c955b3770ecd02f
carissacullen <carissa.cullen@yahoo.com>
2024-08-25 20:53:40 +0100

    added play again button

--- pages/Conclusion.py
+++ pages/Conclusion.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
 
 st.set_page_config(
     page_title="Conclusion",
@@ -17,6 +18,8 @@ st.markdown(
     - example 1
     ## Future Risks
     - risk example
-    ## Play again?
 """
 )
+
+if st.button("Play Again?"):
+    switch_page('main')
