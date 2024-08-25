5cd6a206b00f423a603ffed150b632d88aafba59
carissacullen <carissa.cullen@yahoo.com>
2024-08-25 22:12:07 +0100

    time_up ask for refresh

--- pages/time_up.py
+++ pages/time_up.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
+from streamlit_js_eval import streamlit_js_eval
 
 st.title("⏰ Time's Up!")
 
@@ -8,10 +9,14 @@ st.markdown(
     ### You ran out of time for your assignment.
 
     ### Maybe use the coding assistant next time. 
+
+    Please refresh the page before clicking 'Try Again'
 """)
 
 if st.button("Try Again",type="primary"):
+    remaining_time=180
     switch_page('main')
 
 if st.button("I give up"):
-    switch_page('Conclusion')    
+    switch_page('Conclusion')  
+
